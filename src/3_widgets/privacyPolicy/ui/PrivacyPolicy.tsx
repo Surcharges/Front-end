bf91bd2fdf8e64052c0684fbccdaa070e4fffb7b
Bonsung Koo <developer@bonsung.me>
2025-02-03 10:29:25 +1300

    Privacy Policy has been changed #84

--- src/3_widgets/privacyPolicy/ui/PrivacyPolicy.tsx
+++ src/3_widgets/privacyPolicy/ui/PrivacyPolicy.tsx
@@ -2,14 +2,15 @@ import { Link } from "react-router-dom";
 
 export function PrivacyPolicy() {
   return (
-    <div className="flex flex-col items-center h-screen">
+    <div className="flex flex-col items-center h-screen m-10">
       <p className="text-3xl">Surcharges Privacy Policy</p>
       <div className="text-xl mt-5">
-        <p>The Surcharges service including, iOS App, does not collect any user data of any kind.</p>
+        <p>The Surcharges service including, iOS App, does not collect any user data except purchages receipt image or photo.</p>
         <p>If you wish to verify this for yourself, see the source code through below links</p>
       </div>
       <div className="flex flex-col items-center text-xl underline mt-5">
         <Link to="https://github.com/Surcharges/Front-end">https://github.com/Surcharges/Front-end</Link>
+        <Link to="https://github.com/Surcharges/Back-end">https://github.com/Surcharges/Back-end</Link>
         <Link to="https://github.com/Surcharges/iOS">https://github.com/Surcharges/iOS</Link>
       </div>
     </div>
